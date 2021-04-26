17aeb784d68ae98fa365b77b1f11cfac89a951ca
pj <pjhg14@gmail.com>
2021-04-25 20:53:56 -0400

    commit tree ac1681adc4e3bfd3323f955b44109740b3d63365

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+def prime?(number)
+    if number <= 1 
+        return false
+    end
+
+    if number == 2
+        return true
+    end
+
+    counter = 2
+
+    (number / 2).times do
+        if number % counter == 0
+            return false
+        end
+
+
+        counter += 1
+    end
+
+    return true
+end
